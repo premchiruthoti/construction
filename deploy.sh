aa32a09c731f9092df331ffbdcdeaf1475a15114
premchiruthoti <48217244+premchiruthoti@users.noreply.github.com>
2023-12-30 20:35:35 +0530

    changes made to deployment files

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
-cd /home/ubuntu/deploy_nextjs_app
-git pull origin master
-nvm use v16
-yarn install &&
-yarn build &&
-pm2 restart next_hello_world_app
+cd /home/ubuntu/construction
+git pull origin main
+nvm use v20
+npm install &&
+npm run build &&
+pm2 restart construction
